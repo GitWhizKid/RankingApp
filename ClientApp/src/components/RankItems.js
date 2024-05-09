a28001e8c7611e4592e5f94123032a15424b5b5d
GitWhizKid <forvoxchannel@gmail.com>
2024-05-10 01:22:37 +0300

    Output each of the images to the screen

--- ClientApp/src/components/RankItems.js
+++ ClientApp/src/components/RankItems.js
@@ -1,9 +1,10 @@
 ﻿import React, { useState, useEffect } from 'react';
+import MovieImageArr from "./MovieImages.js";
 
 const RankItems = () => {
 
     const [items, setItems] = useState([]);
-    const dataType = 1;
+    const dataType = 1; 
 
     useEffect(() => {
         fetch(`item/${dataType}`)
@@ -18,9 +19,15 @@ const RankItems = () => {
 
     return (
         <main>
+            <div className = "items-not-ranked">
             {
-                (items.length > 0) ? items.map((item) => <h3>{ item.title}</h3>):<div>Loading...</div>      
+                    (items.length > 0) ? items.map((item) =>
+
+                        <img id={'item-${item.id}'} src={MovieImageArr.find(o=>o.id === item.imageId)?.image } />
+
+                    ) : <div>Loading...</div>      
             }
+            </div>
 
         </main>
     )
