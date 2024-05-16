b9f8bfd48016766fd39bf083e307cdaf609d9011
GitWhizKid <forvoxchannel@gmail.com>
2024-05-17 00:27:36 +0300

    Imported the ItemCollection into the RankItems

--- ClientApp/src/components/RankItems.js
+++ ClientApp/src/components/RankItems.js
@@ -1,6 +1,7 @@
 ﻿import React, { useState, useEffect } from 'react';
 import MovieImageArr from "./MovieImages.js";
 import RankingGrid from "./RankingGrid";
+import ItemCollection from "./ItemCollection";
 
 const RankItems = () => {
 
@@ -41,23 +42,14 @@ const RankItems = () => {
 
     }, []);
 
-    return (
-        <main>
-            <RankingGrid items={items} imgArr={MovieImageArr} drag={drag} allowDrop={allowDrop} drop={drop} />
-            <div className = "items-not-ranked">
-            {
-                    (items.length > 0) ? items.map((item) =>
-                    (item.ranking === 0) ?
-                    <div className = "unranked">
-                            <img id={`item-${item.id}`} src={MovieImageArr.find(o => o.id === item.imageId)?.image}
-                                style={{ cursor: "pointer" }} draggable="true" onDragStart={drag }
-                            />
-                    </div> : null
-                    ) : <div>Loading...</div>      
-            }
-            </div>
+        return (
+            (items != null) ?
+                <main>
+                    <RankingGrid items={items} imgArr={imgArr} drag={drag} allowDrop={allowDrop} drop={drop} />
+                    <ItemCollection items={items} drag={drag} imgArr={imgArr} />
+                </main>
+                : <main>Loading...</main>
+        )
 
-        </main>
-    )
 }
 export default RankItems;
